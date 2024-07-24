d1a6b76edb0a2a4a035d70d7d66c4ddd1c6365d4
Alfredo Pasquel <alfredo.pasquel@gmail.com>
2024-07-23 20:57:33 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee };
+
+    newObj[key] = value;
+
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    return {
+        ...employee,
+    [key]: value,
+  };
+};
+
+const newEmployee = deleteFromEmployeeByKey(
+    employee, 
+    key, 
+    value
+
+);
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+
+
+
+
